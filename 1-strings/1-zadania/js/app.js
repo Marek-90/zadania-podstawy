a6086abeaea89005e90e4ca6b11715beea385ffb
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-07-26 17:20:47 +0200

    Hello

---
+++ 1-strings/1-zadania/js/app.js
@@ -0,0 +1,11 @@
+//------------------ 1 ------------------- //
+
+console.log("Koty to fajne zwierzaki".length)
+
+//------------------ 2 ------------------- //
+
+let name1 = prompt("jak masz na imę?");
+if (name1 == true) {
+
+}
+alert("Witaj " + name1)
